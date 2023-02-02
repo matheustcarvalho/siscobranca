d5603c60d2f4fad955ca65ed497ce0bafcb461e0
matheustdc <2110219@sempre.unifacig.edu.br>
2023-02-02 13:49:42 -0300

    host

--- src/instances/mysql.ts
+++ src/instances/mysql.ts
@@ -9,8 +9,10 @@ export const sequelize = new Sequelize(
     process.env.MYSQL_USER as string,
     process.env.MYSQL_PASSWORD as string,
     {
+        host: process.env.MYSQL_HOST,
         dialect: 'mysql',
-        port: parseInt(process.env.MYSQL_PORT as string)
+        port: parseInt(process.env.MYSQL_PORT as string),
+        
     }
 
 );
